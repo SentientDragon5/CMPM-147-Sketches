a50cc4c14a39b5592ea653e749c27fb7b2d50695
SentientDragon5 <34078184+SentientDragon5@users.noreply.github.com>
2025-04-14 20:38:53 -0700

    add experiment 2

--- experiment2/js/sketch.js
+++ experiment2/js/sketch.js
@@ -1,79 +1,58 @@
-// sketch.js - purpose and description here
-// Author: Your Name
-// Date:
+let color_bottom;
+let color_top;
 
-// Here is how you might set up an OOP p5.js project
-// Note that p5.js looks for a file called sketch.js
+let bubbles = [];
+const bubble_count = 20;
 
-// Constants - User-servicable parts
-// In a longer project I like to put these in a separate file
-const VALUE1 = 1;
-const VALUE2 = 2;
-
-// Globals
-let myInstance;
-let canvasContainer;
-var centerHorz, centerVert;
-
-class MyClass {
-    constructor(param1, param2) {
-        this.property1 = param1;
-        this.property2 = param2;
-    }
-
-    myMethod() {
-        // code to run when method is called
-    }
+class Bubble{
+  constructor(x,y,r){
+    this.x = x || random(width)
+    this.y = y || random(height, height + 100)
+    this.r = r || random(5,30)
+    this.y_vel = random(1,3)
+  }
+  move(){
+    this.y -= this.y_vel
+  }
+  display(){
+    noStroke()
+    fill(255,255,255,100)
+    ellipse(this.x,this.y,this.r*2)
+  }
+  offscreen(){
+    return this.y < -this.r
+  }
 }
 
-function resizeScreen() {
-  centerHorz = canvasContainer.width() / 2; // Adjusted for drawing logic
-  centerVert = canvasContainer.height() / 2; // Adjusted for drawing logic
-  console.log("Resizing...");
-  resizeCanvas(canvasContainer.width(), canvasContainer.height());
-  // redrawCanvas(); // Redraw everything based on new size
-}
-
-// setup() function is called once when the program starts
 function setup() {
-  // place our canvas, making it fit our container
-  canvasContainer = $("#canvas-container");
-  let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
-  canvas.parent("canvas-container");
-  // resize canvas is the page is resized
-
-  // create an instance of the class
-  myInstance = new MyClass("VALUE1", "VALUE2");
-
-  $(window).resize(function() {
-    resizeScreen();
-  });
-  resizeScreen();
+  createCanvas(400, 400);
+  
+  color_bottom = color(24,26,87);
+  color_top = color(108, 130, 230);
+  
+  for(let i=0;i<bubble_count;i++){
+    bubbles.push(new Bubble());
+  }
 }
 
-// draw() function is called repeatedly, it's the main animation loop
 function draw() {
-  background(220);    
-  // call a method on the instance
-  myInstance.myMethod();
-
-  // Set up rotation for the rectangle
-  push(); // Save the current drawing context
-  translate(centerHorz, centerVert); // Move the origin to the rectangle's center
-  rotate(frameCount / 100.0); // Rotate by frameCount to animate the rotation
-  fill(234, 31, 81);
-  noStroke();
-  rect(-125, -125, 250, 250); // Draw the rectangle centered on the new origin
-  pop(); // Restore the original drawing context
-
-  // The text is not affected by the translate and rotate
-  fill(255);
-  textStyle(BOLD);
-  textSize(140);
-  text("p5*", centerHorz - 105, centerVert + 40);
-}
-
-// mousePressed() function is called once after every time a mouse button is pressed
-function mousePressed() {
-    // code to run when mouse is pressed
+  for(let y=0; y<height; y++){
+    n = map(y,0,height,0,1);
+    let bg_color = lerpColor(color_top,color_bottom,n);
+    stroke(bg_color);
+    line(0,y,width, y);
+  }
+  
+  if(random(1) < 0.04){
+    bubbles.push(new Bubble())
+  }
+  
+  for(let i=bubbles.length-1;i>=0;i--){
+    bubbles[i].move();
+    bubbles[i].display();
+    
+    if(bubbles[i].offscreen()){
+      bubbles.splice(i,1);
+    }
+  }
 }
