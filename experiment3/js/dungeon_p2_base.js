121b9401fbe1d326308e3805a06f6889cda5ff7c
SentientDragon5 <34078184+SentientDragon5@users.noreply.github.com>
2025-04-21 00:54:20 -0700

    working experiment 3

--- experiment3/js/dungeon_p2_base.js
+++ experiment3/js/dungeon_p2_base.js
@@ -2,72 +2,279 @@
 
 /* global generateGrid drawGrid */
 
-let seed = 0;
-let tilesetImage;
-let currentGrid = [];
-let numRows, numCols;
+var t = function (p) {
+  let seed = 0;
+  let tilesetImage;
+  let currentGrid = [];
+  let numRows, numCols;
 
-function preload() {
-  tilesetImage = loadImage("./img/tileset.png");
-}
+  p.preload = function () {
+    tilesetImage = p.loadImage("./img/tileset.png");
+  };
 
-function reseed() {
-  seed = (seed | 0) + 1109;
-  randomSeed(seed);
-  noiseSeed(seed);
-  select("#seedReport_2").html("seed " + seed);
-  regenerateGrid();
-}
+  p.reseed = function () {
+    seed = (seed | 0) + 1109;
+    p.randomSeed(seed);
+    p.noiseSeed(seed);
+    p.select("#seedReport_2").html("seed " + seed);
+    p.regenerateGrid();
+  };
 
-function regenerateGrid() {
-  select("#asciiBox_2").value(gridToString(generateGrid(numCols, numRows)));
-  reparseGrid();
-}
+  p.regenerateGrid = function () {
+    p.select("#asciiBox_2").value(
+      p.gridToString(p.generateGrid(numCols, numRows))
+    );
+    p.reparseGrid();
+  };
 
-function reparseGrid() {
-  currentGrid = stringToGrid(select("#asciiBox_2").value());
-}
+  p.reparseGrid = function () {
+    currentGrid = p.stringToGrid(p.select("#asciiBox_2").value());
+  };
 
-function gridToString(grid) {
-  let rows = [];
-  for (let i = 0; i < grid.length; i++) {
-    rows.push(grid[i].join(""));
-  }
-  return rows.join("\n");
-}
-
-function stringToGrid(str) {
-  let grid = [];
-  let lines = str.split("\n");
-  for (let i = 0; i < lines.length; i++) {
-    let row = [];
-    let chars = lines[i].split("");
-    for (let j = 0; j < chars.length; j++) {
-      row.push(chars[j]);
+  p.gridToString = function (grid) {
+    let rows = [];
+    for (let i = 0; i < grid.length; i++) {
+      rows.push(grid[i].join(""));
     }
-    grid.push(row);
-  }
-  return grid;
-}
+    return rows.join("\n");
+  };
 
-function setup() {
-  numCols = select("#asciiBox_2").attribute("rows") | 0;
-  numRows = select("#asciiBox_2").attribute("cols") | 0;
+  p.stringToGrid = function (str) {
+    let grid = [];
+    let lines = str.split("\n");
+    for (let i = 0; i < lines.length; i++) {
+      let row = [];
+      let chars = lines[i].split("");
+      for (let j = 0; j < chars.length; j++) {
+        row.push(chars[j]);
+      }
+      grid.push(row);
+    }
+    return grid;
+  };
 
-  createCanvas(16 * numCols, 16 * numRows).parent("canvasContainer_2");
-  select("canvas").elt.getContext("2d").imageSmoothingEnabled = false;
+  p.setup = function () {
+    numCols = p.select("#asciiBox_2").attribute("rows") | 0;
+    numRows = p.select("#asciiBox_2").attribute("cols") | 0;
 
-  select("#reseedButton_2").mousePressed(reseed);
-  select("#asciiBox_2").input(reparseGrid);
+    p.createCanvas(16 * numCols, 16 * numRows).parent("canvasContainer_2");
+    p.select("canvas").elt.getContext("2d").imageSmoothingEnabled = false;
 
-  reseed();
-}
+    p.select("#reseedButton_2").mousePressed(p.reseed);
+    p.select("#asciiBox_2").input(p.reparseGrid);
 
-function draw() {
-  randomSeed(seed);
-  drawGrid(currentGrid);
-}
+    p.reseed();
+  };
 
-function placeTile(i, j, ti, tj) {
-  image(tilesetImage, 16 * j, 16 * i, 16, 16, 8 * ti, 8 * tj, 8, 8);
-}
+  p.draw = function () {
+    p.randomSeed(seed);
+    p.drawGrid(currentGrid);
+  };
+
+  p.placeTile = function (i, j, ti, tj) {
+    p.image(tilesetImage, 16 * j, 16 * i, 16, 16, 8 * ti, 8 * tj, 8, 8);
+  };
+
+  p.generateGrid = function (numCols, numRows) {
+    let grid = [];
+    for (let i = 0; i < numRows; i++) {
+      let row = [];
+      for (let j = 0; j < numCols; j++) {
+        row.push("_");
+      }
+      grid.push(row);
+    }
+
+    // carve rooms
+
+    let cellsRow = 3;
+    let cellsCol = 3;
+    let pad = 0;
+
+    let maxCellW = p.floor(numRows / cellsCol);
+    let maxCellH = p.floor(numCols / cellsRow);
+
+    let rectGrid = [];
+
+    for (let k = 0; k < cellsCol; k++) {
+      let rectRow = [];
+      for (let l = 0; l < cellsRow; l++) {
+        let cellW = p.floor(p.random(maxCellW, 3)) - pad * 2;
+        let cellH = p.floor(p.random(maxCellH, 3)) - pad * 2;
+        let cellX = p.floor(p.random(0, 3)) + maxCellW * l + pad;
+        let cellY = p.floor(p.random(0, 3)) + maxCellH * k + pad;
+        rectRow.push([cellX, cellY, cellW, cellH]);
+        for (let i = 0; i < cellH; i++) {
+          for (let j = 0; j < cellW; j++) {
+            grid[i + cellY][j + cellX] = ".";
+          }
+        }
+
+        let poiX = p.floor(p.random(cellX + 1, cellX + cellW - 1));
+        let poiY = p.floor(p.random(cellY + 1, cellY + cellH - 1));
+        if (p.random(1) > 0.6) {
+          grid[poiY][poiX] = "C";
+        }
+
+        if (k > 0) {
+          let doorX = p.floor(p.random(cellX + 1, cellX + cellW - 1));
+          let doorY = cellY;
+
+          let room = rectGrid[k - 1][l];
+          let otherDoorX = doorX;
+          let otherDoorY = room[1] + room[3] - 1;
+
+          for (let i = otherDoorY - 1; i <= doorY; i++) {
+            for (let j = otherDoorX; j <= doorX; j++) {
+              grid[i][j] = "=";
+            }
+          }
+
+          if (p.random(1) > 0.5) {
+            grid[doorY][doorX] = "D";
+          }
+          if (p.random(1) > 0.5) {
+            grid[otherDoorY][otherDoorX] = "D";
+          }
+        }
+
+        if (l > 0) {
+          let doorX = cellX;
+          let doorY = p.floor(p.random(cellY + 1, cellY + cellH - 1));
+
+          let room = rectRow[l - 1];
+          let otherDoorX = room[0] + room[3] - 1;
+          let otherDoorY = doorY;
+
+          for (let i = doorY; i <= otherDoorY; i++) {
+            for (let j = doorX - 1; j <= otherDoorX; j++) {
+              grid[i][j] = "=";
+            }
+          }
+
+          if (p.random(1) > 0.5) {
+            grid[doorY][doorX] = "D";
+          }
+          if (p.random(1) > 0.5) {
+            grid[otherDoorY][otherDoorX] = "D";
+          }
+        }
+        rectGrid.push(rectRow);
+      }
+    }
+
+    return grid;
+  };
+
+  p.drawGrid = function (grid) {
+    p.background(0);
+
+    for (let i = 0; i < grid.length; i++) {
+      for (let j = 0; j < grid[i].length; j++) {
+        if (p.gridCheck(grid, i, j, ".") || p.gridCheck(grid, i, j, "_")) {
+          const bg = randBG[p.random(randBG.length) | 0];
+          p.placeTile(i, j, bg[0], bg[1]);
+        } else if (p.gridCheck(grid, i, j, "=")) {
+          const bg = randPath[p.random(randPath.length) | 0];
+          p.placeTile(i, j, bg[0], bg[1]);
+        }
+
+        if (p.gridCheck(grid, i, j, "_")) {
+          const bg = randPurple[p.random(randPurple.length) | 0];
+          p.placeTile(i, j, bg[0], bg[1]);
+        } else {
+          p.drawContext(grid, i, j, "_", 0, 0, dungeonEdge);
+        }
+
+        if (p.gridCheck(grid, i, j, "C")) {
+          p.placeTile(i, j, p.random(6) | 0, (p.random(3) | 0) + 28);
+        }
+        if (p.gridCheck(grid, i, j, "D")) {
+          p.placeTile(i, j, 5, 25 + (p.random(3) | 0));
+        }
+      }
+    }
+  };
+
+  p.gridCheck = function (grid, i, j, target) {
+    if (i >= 0 && i < grid.length && j >= 0 && j < grid[0].length) {
+      return grid[i][j] == target;
+    }
+
+    return false;
+  };
+
+  p.gridCode = function (grid, i, j, target) {
+    const n = p.gridCheck(grid, i + 1, j, target);
+    const s = p.gridCheck(grid, i - 1, j, target);
+    const e = p.gridCheck(grid, i, j + 1, target);
+    const w = p.gridCheck(grid, i, j - 1, target);
+    return (n << 0) + (s << 1) + (w << 2) + (e << 3);
+  };
+
+  p.drawContext = function (grid, i, j, target, dti, dtj, lookup) {
+    const tileCode = p.gridCode(grid, i, j, target);
+    const w = lookup[tileCode];
+    if (tileCode != 0) {
+      p.placeTile(i, j, w[0], w[1]);
+    }
+    //p.placeTile(i, j, dti, dtj);
+  };
+
+  const randBG = [
+    [26, 24],
+    [21, 24],
+    [21, 23],
+    [21, 22],
+    [21, 21],
+    [20, 23],
+    [20, 23],
+    [20, 23],
+    [20, 23],
+  ];
+
+  const randPath = [
+    [20, 22],
+    [20, 22],
+    [20, 22],
+    [20, 22],
+    [20, 22],
+    [26, 24],
+    [27, 24],
+    [28, 24],
+    [26, 24],
+  ];
+
+  const randPurple = [
+    [6, 24],
+    [1, 24],
+    [1, 23],
+    [1, 22],
+    [1, 21],
+    [0, 23],
+    [0, 23],
+    [0, 23],
+    [0, 23],
+  ];
+
+  const dungeonEdge = [
+    [6, 22], //surrounded
+    [6, 23], //n
+    [6, 21], //s
+    [6, 22], //ns
+    [5, 22], //e
+    [5, 23], //ne
+    [5, 21], //se
+    [1, 24], //nse
+    [7, 22], //w
+    [7, 23], //nw
+    [7, 21], //sw
+    [1, 24], //nsw
+    [1, 24], //ew
+    [1, 24], //new
+    [1, 24], //sew
+    [1, 22], //single
+  ];
+};
+
+var myp5 = new p5(t, "c2");
--- experiment3/js/p2_base.js
+++ experiment3/js/p2_base.js
@@ -2,72 +2,268 @@
 
 /* global generateGrid drawGrid */
 
-let seed = 0;
-let tilesetImage;
-let currentGrid = [];
-let numRows, numCols;
+var s = function (p) {
+  let seed = 0;
+  let tilesetImage;
+  let currentGrid = [];
+  let numRows, numCols;
 
-function preload() {
-  tilesetImage = loadImage("./img/tileset.png");
-}
+  p.preload = function () {
+    tilesetImage = p.loadImage("./img/tileset.png");
+  };
 
-function reseed() {
-  seed = (seed | 0) + 1109;
-  randomSeed(seed);
-  noiseSeed(seed);
-  select("#seedReport").html("seed " + seed);
-  regenerateGrid();
-}
+  p.reseed = function () {
+    seed = (seed | 0) + 1109;
+    p.randomSeed(seed);
+    p.noiseSeed(seed);
+    p.select("#seedReport").html("seed " + seed);
+    p.regenerateGrid();
+  };
 
-function regenerateGrid() {
-  select("#asciiBox").value(gridToString(generateGrid(numCols, numRows)));
-  reparseGrid();
-}
+  p.regenerateGrid = function () {
+    p.select("#asciiBox").value(
+      p.gridToString(p.generateGrid(numCols, numRows))
+    );
+    p.reparseGrid();
+  };
 
-function reparseGrid() {
-  currentGrid = stringToGrid(select("#asciiBox").value());
-}
+  p.reparseGrid = function () {
+    currentGrid = p.stringToGrid(p.select("#asciiBox").value());
+  };
 
-function gridToString(grid) {
-  let rows = [];
-  for (let i = 0; i < grid.length; i++) {
-    rows.push(grid[i].join(""));
-  }
-  return rows.join("\n");
-}
-
-function stringToGrid(str) {
-  let grid = [];
-  let lines = str.split("\n");
-  for (let i = 0; i < lines.length; i++) {
-    let row = [];
-    let chars = lines[i].split("");
-    for (let j = 0; j < chars.length; j++) {
-      row.push(chars[j]);
+  p.gridToString = function (grid) {
+    let rows = [];
+    for (let i = 0; i < grid.length; i++) {
+      rows.push(grid[i].join(""));
     }
-    grid.push(row);
+    return rows.join("\n");
+  };
+
+  p.stringToGrid = function (str) {
+    let grid = [];
+    let lines = str.split("\n");
+    for (let i = 0; i < lines.length; i++) {
+      let row = [];
+      let chars = lines[i].split("");
+      for (let j = 0; j < chars.length; j++) {
+        row.push(chars[j]);
+      }
+      grid.push(row);
+    }
+    return grid;
+  };
+
+  p.setup = function () {
+    numCols = p.select("#asciiBox").attribute("rows") | 0;
+    numRows = p.select("#asciiBox").attribute("cols") | 0;
+
+    p.createCanvas(16 * numCols, 16 * numRows).parent("canvasContainer");
+    p.select("canvas").elt.getContext("2d").imageSmoothingEnabled = false;
+
+    p.select("#reseedButton").mousePressed(p.reseed);
+    p.select("#asciiBox").input(p.reparseGrid);
+
+    p.reseed();
+  };
+
+  p.draw = function () {
+    p.randomSeed(seed);
+    p.drawGrid(currentGrid);
+  };
+
+  p.placeTile = function (i, j, ti, tj) {
+    p.image(tilesetImage, 16 * j, 16 * i, 16, 16, 8 * ti, 8 * tj, 8, 8);
+  };
+
+  /* exported generateGrid, drawGrid */
+  /* global p.placeTile */
+
+  const noiseScale = 1 / 12;
+
+  p.generateGrid = function (numCols, numRows) {
+    let grid = [];
+    for (let i = 0; i < numRows; i++) {
+      let row = [];
+      for (let j = 0; j < numCols; j++) {
+        let n = p.noise(i * noiseScale, j * noiseScale);
+        if (p.round(n * 100 * 3) == 56 * 3) {
+          row.push("h");
+        } else if (n > 0.6) {
+          row.push("t");
+        } else if (n > 0.5) {
+          row.push("_");
+        } else {
+          row.push("~");
+        }
+      }
+      grid.push(row);
+    }
+
+    Snowflake.create(snowflakes, snowflake_count);
+    return grid;
+  };
+
+  p.drawGrid = function (grid) {
+    p.background(128);
+
+    for (let i = 0; i < grid.length; i++) {
+      for (let j = 0; j < grid[i].length; j++) {
+        // Water under layer
+        p.placeTile(i, j, p.random(4) | 0, 13);
+
+        // place grass
+        if (
+          p.gridCheck(grid, i, j, "_") ||
+          p.gridCheck(grid, i, j, "t") ||
+          p.gridCheck(grid, i, j, "h")
+        ) {
+          p.placeTile(i, j, p.random(4) | 0, 12);
+        } else {
+          // place cliffs
+          p.drawContext(grid, i, j, "_", 5, 0, snowyCliffsOverWater);
+        }
+
+        if (p.gridCheck(grid, i, j, "t")) {
+          p.drawContext(grid, i, j, "t", 0, 0, snowytrees);
+        }
+        if (p.gridCheck(grid, i, j, "h")) {
+          p.placeTile(i, j, 26 + (p.random(2) | 0), p.random(4) | 0);
+        }
+      }
+    }
+
+    Snowflake.update_arr(snowflakes);
+    Snowflake.draw_arr(snowflakes);
+  };
+
+  p.gridCheck = function (grid, i, j, target) {
+    if (i >= 0 && i < grid.length && j >= 0 && j < grid[0].length) {
+      return grid[i][j] == target;
+    }
+
+    return false;
+  };
+
+  p.gridCode = function (grid, i, j, target) {
+    const n = p.gridCheck(grid, i + 1, j, target);
+    const s = p.gridCheck(grid, i - 1, j, target);
+    const e = p.gridCheck(grid, i, j + 1, target);
+    const w = p.gridCheck(grid, i, j - 1, target);
+    return (n << 0) + (s << 1) + (w << 2) + (e << 3);
+  };
+
+  p.drawContext = function (grid, i, j, target, dti, dtj, lookup) {
+    const tileCode = p.gridCode(grid, i, j, target);
+    const w = lookup[tileCode];
+    if (tileCode != 0) {
+      p.placeTile(i, j, w[0], w[1]);
+    }
+    //placeTile(i, j, dti, dtj);
+  };
+
+  const cliffsOverWater = [
+    [1, 13], //surrounded
+    [10, 2], //n
+    [10, 0], //s
+    [1, 13], //ns
+    [9, 1], //e
+    [9, 2], //ne
+    [9, 0], //se
+    [9, 1], //nse
+    [11, 1], //w
+    [13, 0], //nw
+    [11, 0], //sw
+    [11, 1], //nsw
+    [1, 13], //ew
+    [10, 2], //new
+    [10, 0], //sew
+    [1, 13], //single
+  ];
+
+  const snowyCliffsOverWater = [
+    [1, 13], //surrounded
+    [10, 14], //n
+    [10, 12], //s
+    [1, 13], //ns
+    [9, 13], //e
+    [9, 14], //ne
+    [9, 12], //se
+    [9, 13], //nse
+    [11, 13], //w
+    [13, 12], //nw
+    [11, 12], //sw
+    [11, 13], //nsw
+    [1, 13], //ew
+    [10, 14], //new
+    [10, 12], //sew
+    [1, 13], //single
+  ];
+
+  const snowytrees = [
+    [22, 7], //surrounded
+    [22, 6], //n
+    [22, 8], //s
+    [22, 7], //ns
+    [23, 7], //e
+    [23, 6], //ne
+    [23, 8], //se
+    [22, 7], //nse
+    [21, 7], //w
+    [21, 6], //nw
+    [21, 8], //sw
+    [22, 7], //nsw
+    [22, 7], //ew
+    [22, 7], //new
+    [10, 12], //sew
+    [20, 6], //single
+  ];
+
+  // Snow
+  let snowflakes = [];
+  let snowflake_count = 300;
+
+  class Snowflake {
+    static create(arr, count) {
+      for (let i = 0; i < count; i++) {
+        arr.push(new Snowflake());
+      }
+    }
+    static update_arr(arr) {
+      for (let i = 0; i < arr.length; i++) {
+        arr[i].update();
+      }
+    }
+    static draw_arr(arr) {
+      for (let i = 0; i < arr.length; i++) {
+        arr[i].draw();
+      }
+    }
+
+    constructor() {
+      this.reset();
+      this.y = p.random(p.height);
+    }
+    reset() {
+      this.x = p.random(p.width);
+      this.y = p.random(-10, -50); // above screen
+      this.r = p.random(2, 6);
+      this.dy = p.map(this.r, 2, 6, 0.5, 2.8); // make y velocity determined on size
+      this.dx = p.random(-0.3, 0.3);
+    }
+    update() {
+      this.x += this.dx;
+      this.y += this.dy;
+
+      if (this.y > p.height + this.r) {
+        return true;
+      }
+      return false;
+    }
+    draw() {
+      p.noStroke();
+      p.rect(this.x, this.y, this.r, this.r);
+    }
   }
-  return grid;
-}
+};
 
-function setup() {
-  numCols = select("#asciiBox").attribute("rows") | 0;
-  numRows = select("#asciiBox").attribute("cols") | 0;
-
-  createCanvas(16 * numCols, 16 * numRows).parent("canvasContainer");
-  select("canvas").elt.getContext("2d").imageSmoothingEnabled = false;
-
-  select("#reseedButton").mousePressed(reseed);
-  select("#asciiBox").input(reparseGrid);
-
-  reseed();
-}
-
-function draw() {
-  randomSeed(seed);
-  drawGrid(currentGrid);
-}
-
-function placeTile(i, j, ti, tj) {
-  image(tilesetImage, 16 * j, 16 * i, 16, 16, 8 * ti, 8 * tj, 8, 8);
-}
+var myp5 = new p5(s, "c1");
